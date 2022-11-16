6a21107670966ac41dbf104c1a962584af361c4b
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-16 00:59:16 -0800

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Starts Flask web app
-Listening on 0.0.0.0:5000
-Route '/' displays "Hello HBNB!"
-=======
-"""Starts Flask web app that displays "Hello HBNB!"
->>>>>>> 052ab6c5e78d1867e6f9815110ae00acbabf44f0
-"""
+"""Starts a Flask web app listening on 0.0.0.0:5000"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
-    """Displays 'Hello HBNB!'"""
+def hello_flask():
+    """Displays “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-web flask as my container
-=======
 # 0x04. AirBnB clone - Web framework
->>>>>>> 052ab6c5e78d1867e6f9815110ae00acbabf44f0
